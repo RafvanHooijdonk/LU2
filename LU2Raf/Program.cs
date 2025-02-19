ec2f2674efa4522b6aa51d09339a58a28118f2d8
Raf <raf.van.hooijdonk@gmail.com>
2025-02-19 15:47:11 +0100

    main

--- LU2Raf/Program.cs
+++ LU2Raf/Program.cs
@@ -6,12 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-var sqlConnectionString = builder.Configuration["SqlConnectionString"];
+//var sqlConnectionString = builder.Configuration["SqlConnectionString"];
+var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString"); var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
+
 builder.Services.AddTransient<IEnvironment2DRepository>(o => new Environment2DRepository(sqlConnectionString));
 builder.Services.AddTransient<IObject2DRepository>(o => new Object2DRepository(sqlConnectionString));
 
 var app = builder.Build();
 app.MapGet("/", () => "Hello world, the API is up!");
+app.MapGet("/", () => $"The API is up . Connection string found: {(sqlConnectionStringFound ? "Yes" : "No")}");
 
 if (app.Environment.IsDevelopment())
 {
