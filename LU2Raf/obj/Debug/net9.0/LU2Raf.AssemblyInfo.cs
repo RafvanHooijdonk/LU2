5c747513d2bdbcd0044530409030ba9981b47353
Raf <raf.van.hooijdonk@gmail.com>
2025-02-26 16:56:35 +0100

    main

--- LU2Raf/obj/Debug/net9.0/LU2Raf.AssemblyInfo.cs
+++ LU2Raf/obj/Debug/net9.0/LU2Raf.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LU2Raf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416fe14132ff97d9fd89b9350aeb01523e12963b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb780ede81a5509d355e69ef74575d6425e6d4e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LU2Raf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LU2Raf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
